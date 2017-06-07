d8604d88435eeff8a5a33455dccd528a100dc60e
ie702272 <ie702272@iteso.mx>
2017-06-07 10:44:54 -0500

    CasiTodoHanoi
    SergioChung

--- Practica1.asm
+++ Practica1.asm
@@ -9,7 +9,7 @@ MAIN:
 	addi $t1, $t0, 4 		# Columna B
 	addi $t2, $t1, 4 		# Columna C
 	and $s6, $s7, 1		# Para ver si es par o non
-	beq $s6, 1, HANOI
+	beq $s6, 1, INICIO
 	lw $s1, 0($sp)		# Guardamos valor de la columna inicial a s1
 	j COLB			# Jump to Main label
 	
@@ -24,42 +24,52 @@ INTRODUCIR:
 	addi $t0, $t0, 0x20		# Avanzar fila
 	j IDATOS
 
-HANOI:	
+INICIO:	
 	lw $s1, 0($sp)		# Guardamos valor de la columna inicial a s1
-	beq $s1, $s7, FINAL		# Si ya llegamos al último valor agregado
- COLC:	lw $t7, 0($t2)
-	bne $t7, $zero, COLB 	# Revisar columna C
+ COLC:	lw $t7, 0($t2)		#Cargamos lo que hay en t2 a t7
+	bne $t7, $zero, COLB 	# Si t2 tiene un valor revisar columna B
 	sw $s1, 0($t2)
-	sub $s5, $s1, 1
-	beq $s5, 1, ESDOS
+	bne $s1, $zero, CERO
+	addi $s3, $s3, 3
 	j MOV
- COLB:	lw $t7, 0($t1)		# Revisar columna B
-	bne $t7, $zero, NIVELAR
+ COLB:	lw $t7, 0($t1)		# Cargar valor de
+	bne $t7, $zero, HANOI
 	sw $s1, 0($t1)
-	sub $s5, $s1, 1
-	beq $s5, 1, ESDOS
+  CERO:	addi $s3, $s3, 2
 	j MOV	
+	
+HANOI:	lw $s1, 0($sp)		# Guardamos valor de la columna inicial a s1
+	beq $t2, $zero, COLC
+	beq $t1, $zero, COLB
+	and $s6, $s1, 1		# Para ver si es par o non. 1 es impar
+	beq $s6, 1, RESTAR
+	jal SUMAR
 
-NIVELAR:	
-	#COMO SABER QUE EL NUMERO ES MAYOR. CON UNA RESTA?
-	#j UNO
+COLI: 	
 	addi $t2, $t2, -0x20
 	addi $t1, $t1, -0x20
-	j COLC
-
-ESDOS:	lw 1, -32(t
+	beq $s3, 1, UNO
+	beq $s3, 2, DOS
+	beq $s3, 3, tres
 	
+SUMAR:	addi $s3, $s3, 1
+	beq $s3, 4, CAMB
+	jal COLI
+	
+RESTAR: 	beq $s3, 1, NADA
+	addi $s3, $s3, -1
+  NADA: 	jal COLI
+	
+CAMB:	addi $s3, $s3, -2
+
+UNO:	add $t5, $t5, $t0
+DOS:	add $t5, $t5, $t1
+TRES:	add $t5, $t5, $t2
+
+
+		
 MOV: 
 	sw $zero, 0($sp)
 	addi $sp, $sp, 0x20
 	j HANOI
-MOV2:	
-	sw $s1, 0($t0)
-
-FINAL:    
-	
-	sw $s1, 0($t2)		# Movemos ultimo valor a la columna C
-	sw $zero, 0($sp)		# Borramos el valor de la columna A
-	j COLC			# Continuamos llenando la columna
-	
 EXIT:
